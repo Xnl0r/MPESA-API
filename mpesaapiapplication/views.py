690e08018f45bc97d9a4107953aecd185d7f8f46
xnl0r <paulchola100@gmail.com>
2024-03-19 15:30:11 +0300

    first commit

---
+++ mpesaapiapplication/views.py
@@ -0,0 +1,19 @@
+from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from django_daraja.mpesa.core import MpesaClient
+# Create your views here.
+def index(request):
+    cl = MpesaClient()
+    phone_number = '0757805250'
+    amount =1
+    account_reference ='reference'
+    transaction_desc = 'Description'
+    callback_url = 'https://datajambili.herokuapp.com/express-payment'
+    response = cl.stk_push(phone_number,amount,account_reference,transaction_desc,callback_url)
+    return redirect('/buy/')
+def stk_push_callback(request):
+    data =request.body
+
+    return HttpResponse("STK push in django")
+def buy(request):
+    return render(request, 'buy.html')
